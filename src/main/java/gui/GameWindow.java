94ecd67b9cb0b44a500eaccbef6e7b912310edc5
onemonday <karakyrt13534@gmail.com>
2023-03-21 16:34:26 +0500

    Добавил реализацию интерфейса Saveable для сохранения состояния

--- src/main/java/gui/GameWindow.java
+++ src/main/java/gui/GameWindow.java
@@ -1,13 +1,18 @@
 package gui;
 
-import java.awt.BorderLayout;
+import save_logic.Saveable;
+import save_logic.State;
+
+import java.awt.*;
+import java.beans.PropertyVetoException;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 
-public class GameWindow extends JInternalFrame
+public class GameWindow extends JInternalFrame implements Saveable
 {
     private final GameVisualizer m_visualizer;
+
     public GameWindow() 
     {
         super("Игровое поле", true, true, true, true);
@@ -16,5 +21,46 @@ public class GameWindow extends JInternalFrame
         panel.add(m_visualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
         pack();
+    }
+
+    @Override
+    public State getState() {
+        State state = new State();
+        state.putElement("prefix", this.getPrefix());
+
+        state.putElement("height", this.getSize().height);
+        state.putElement("width", this.getSize().width);
+
+        state.putElement("location_x", this.getLocation().getX());
+        state.putElement("location_y", this.getLocation().getY());
+
+        state.putElement("is_hidden", this.isIcon);
+
+        return state;
+    }
+
+    @Override
+    public void restoreState(State state) {
+        var height = state.getElement("height");
+        var width = state.getElement("width");
+
+        var locationX = Math.round((Double) state.getElement("location_x"));
+        var locationY = Math.round((Double) state.getElement("location_y"));
+
+        var isHidden = state.getElement("is_hidden");
+
+        this.setSize(Math.toIntExact((Long) width), Math.toIntExact((Long) height));
+        this.setLocation(Math.toIntExact(locationX), Math.toIntExact(locationY));
+
+        try {
+            this.setIcon((Boolean) isHidden);
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getPrefix() {
+        return "GameWindow";
     }
 }
--- src/main/java/gui/LogWindow.java
+++ src/main/java/gui/LogWindow.java
@@ -1,8 +1,7 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.TextArea;
+import java.awt.*;
+import java.beans.PropertyVetoException;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
@@ -10,8 +9,10 @@ import javax.swing.JPanel;
 import log.LogChangeListener;
 import log.LogEntry;
 import log.LogWindowSource;
+import save_logic.Saveable;
+import save_logic.State;
 
-public class LogWindow extends JInternalFrame implements LogChangeListener
+public class LogWindow extends JInternalFrame implements LogChangeListener, Saveable
 {
     private LogWindowSource m_logSource;
     private TextArea m_logContent;
@@ -46,5 +47,47 @@ public class LogWindow extends JInternalFrame implements LogChangeListener
     public void onLogChanged()
     {
         EventQueue.invokeLater(this::updateLogContent);
+    }
+
+    @Override
+    public State getState() {
+        State state = new State();
+        state.putElement("prefix", this.getPrefix());
+
+        state.putElement("height", this.getSize().height);
+        state.putElement("width", this.getSize().width);
+
+        state.putElement("location_x", this.getLocation().getX());
+        state.putElement("location_y", this.getLocation().getY());
+
+        state.putElement("is_hidden", this.isIcon);
+
+        return state;
+    }
+
+    @Override
+    public void restoreState(State state) {
+        var height = state.getElement("height");
+        var width = state.getElement("width");
+
+        var locationX = Math.round((Double) state.getElement("location_x"));
+        var locationY = Math.round((Double) state.getElement("location_y"));
+
+        var isHidden = state.getElement("is_hidden");
+
+        System.out.println(width);
+        this.setSize(Math.toIntExact((Long) width), Math.toIntExact((Long) height));
+        this.setLocation(Math.toIntExact(locationX), Math.toIntExact(locationY));
+
+        try {
+            this.setIcon((Boolean) isHidden);
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getPrefix() {
+        return "LogWindow";
     }
 }
