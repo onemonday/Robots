a3c5ada172f8cf02cbb98a9821eebd8952462567
onemonday <karakyrt13534@gmail.com>
2023-04-03 23:26:51 +0500

    Реализовал модель робота, отвечающую за движение (Model)

---
+++ src/main/java/model/RobotModel.java
@@ -0,0 +1,108 @@
+package model;
+
+import java.util.Observable;
+
+public class RobotModel extends Observable {
+    public static final Integer ROBOT_MOVED = 1;
+    private static final double MAX_VELOCITY = 0.1;
+    private static final double MAX_ANGULAR_VELOCITY = 0.001;
+    private volatile double m_robotPositionX = 100;
+    private volatile double m_robotPositionY = 100;
+    private volatile double m_robotDirection = 0;
+
+    public double getRobotPositionX() {
+        return m_robotPositionX;
+    }
+
+    public double getRobotPositionY() {
+        return m_robotPositionY;
+    }
+
+    public double getRobotDirection() {
+        return m_robotDirection;
+    }
+
+    private static double distance(double x1, double y1, double x2, double y2)
+    {
+        double diffX = x1 - x2;
+        double diffY = y1 - y2;
+        return Math.sqrt(diffX * diffX + diffY * diffY);
+    }
+
+    private static double angleTo(double fromX, double fromY, double toX, double toY)
+    {
+        double diffX = toX - fromX;
+        double diffY = toY - fromY;
+
+        return asNormalizedRadians(Math.atan2(diffY, diffX));
+    }
+
+    private static double asNormalizedRadians(double angle)
+    {
+
+        while (angle < 0)
+        {
+            angle += 2*Math.PI;
+        }
+        while (angle >= 2*Math.PI)
+        {
+            angle -= 2*Math.PI;
+        }
+        return angle;
+    }
+
+    private static double applyLimits(double value, double min, double max)
+    {
+        if (value < min)
+            return min;
+        if (value > max)
+            return max;
+        return value;
+    }
+
+    public void moveRobot(int targetPositionX, int targetPositionY, double duration)
+    {
+        double distance = distance(targetPositionX, targetPositionY,
+                m_robotPositionX, m_robotPositionY);
+        if (distance < 0.5)
+        {
+            return;
+        }
+        double velocity = MAX_VELOCITY;
+        double angleToTarget = angleTo(m_robotPositionX, m_robotPositionY, targetPositionX, targetPositionY);
+        double angularVelocity = 0;
+        if (angleToTarget > m_robotDirection)
+        {
+            angularVelocity = MAX_ANGULAR_VELOCITY;
+        }
+        if (angleToTarget < m_robotDirection)
+        {
+            angularVelocity = -MAX_ANGULAR_VELOCITY;
+        }
+
+        velocity = applyLimits(velocity, 0, MAX_VELOCITY);
+        angularVelocity = applyLimits(angularVelocity, -MAX_ANGULAR_VELOCITY, MAX_ANGULAR_VELOCITY);
+        double newX = m_robotPositionX + velocity / angularVelocity *
+                (Math.sin(m_robotDirection  + angularVelocity * duration) -
+                        Math.sin(m_robotDirection));
+        if (!Double.isFinite(newX))
+        {
+            newX = m_robotPositionX + velocity * duration * Math.cos(m_robotDirection);
+        }
+        double newY = m_robotPositionY - velocity / angularVelocity *
+                (Math.cos(m_robotDirection  + angularVelocity * duration) -
+                        Math.cos(m_robotDirection));
+        if (!Double.isFinite(newY))
+        {
+            newY = m_robotPositionY + velocity * duration * Math.sin(m_robotDirection);
+        }
+        m_robotPositionX = newX;
+        m_robotPositionY = newY;
+        double newDirection = asNormalizedRadians(m_robotDirection + angularVelocity * duration);
+        m_robotDirection = newDirection;
+
+        setChanged();
+        notifyObservers(ROBOT_MOVED);
+        clearChanged();
+    }
+}
